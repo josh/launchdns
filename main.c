698ceaf64f5e1e95bd173d8159805d943445713c
Joshua Peek <josh@joshpeek.com>
2015-01-07 02:12:41 -0800

    Sort switch order

--- main.c
+++ main.c
@@ -59,14 +59,14 @@ int main(int argc, char **argv)
 			case 's':
 				name = optarg;
 				break;
-			case 'p':
-				addr.sin_port = htons(atoi(optarg));
-				break;
 			case 't':
 				timeout = atoi(optarg);
 				break;
 				case '4':
 				inet_pton(AF_INET, optarg, &a);
+				break;
+			case 'p':
+				addr.sin_port = htons(atoi(optarg));
 				break;
 			case '6':
 				inet_pton(AF_INET6, optarg, &aaaa);
